b4c0b3113a89c49bd42aebb12ebb8832a2fa6714
samogoljat <samo.goljat1@gmail.com>
2022-12-15 06:47:40 +0100

    Initial commit

---
+++ react_app/my-app/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
